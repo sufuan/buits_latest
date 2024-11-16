868d3de6517d3c628bfe24c7878d31e2a2fa582e
sufuan <abusufian49539@gmail.com>
2024-11-16 16:17:35 +0600

    add some

--- resources/views/partials/brand_slider.blade.php
+++ resources/views/partials/brand_slider.blade.php
@@ -14,6 +14,12 @@
         <div class="single-logo">
             <img alt="" src="https://scontent.fjsr17-1.fna.fbcdn.net/v/t39.30808-6/309180480_195376212856809_381673162411675256_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGCklTgEUdaySuDQlJ7pbfiCDHS1gjiO8QIMdLWCOI7xOFn5JsDjoRc59Px8fm_WtNWpufh7623e9Gfp2ftldTn&_nc_ohc=jIQ1_vbf_W4Q7kNvgFZehcS&_nc_ht=scontent.fjsr17-1.fna&_nc_gid=Aqa1fnIv54UCoizQWY77-Q1&oh=00_AYC5FjHONMc_tB1ZPiti504qVBrMfqzrGjHPtraZxH4Djg&oe=6723E0BF">
         </div>
+        <div class="single-logo">
+            <img alt=""  src="https://ihcantabria.com/wp-content/uploads/2019/01/ju-UNDP-logo.png" width="100%">
+        </div>
+        <div class="single-logo">
+            <img alt=""  src="{{ asset('assets/landing/img/gp.jpg') }}">
+        </div>
     </div>
 </section>
     
